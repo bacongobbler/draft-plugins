eb82dc1029d5762f1ef0258f517ac7a57c114d1b
Matthew Fisher <matt.fisher@microsoft.com>
2018-05-17 14:55:58 -0700

    add executable path

--- Plugins/pack-repo.lua
+++ Plugins/pack-repo.lua
@@ -12,18 +12,21 @@ plugin = {
             arch = "amd64",
             url = "https://azuredraft.blob.core.windows.net/draft/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
             sha256 = "b6169582b3204ee6eb23640728c91c7eddfecb1d64976c09ae2959d67d3b658f",
+            path = "darwin-amd64/" .. name,
         },
         {
             os = "linux",
             arch = "amd64",
             url = "https://azuredraft.blob.core.windows.net/draft/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
             sha256 = "4cbab465ce2a53eefe918f6eccc604155354228759f433f94b1b9704ae8150a2",
+            path = "linux-amd64/" .. name,
         },
         {
             os = "windows",
             arch = "amd64",
             url = "https://azuredraft.blob.core.windows.net/draft/" .. name .. "-v" .. version .. "-windows-amd64.tar.gz",
             sha256 = "fd07836a937a1afebd6e834bd487d84c8b9def78b9012286b179bc54d4bfe763",
+            path = "windows-amd64/" .. name,
         }
     }
 }
