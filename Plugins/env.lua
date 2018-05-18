454acd19b72290d7dad56876230e5775aadacd42
Matthew Fisher <matt.fisher@microsoft.com>
2018-05-17 23:35:23 -0700

    add env plugin

---
+++ Plugins/env.lua
@@ -0,0 +1,33 @@
+local name = "env"
+local version = "2.0.0"
+
+plugin = {
+    name = name,
+    description = "Displays all available environment variables when writing a Draft plugin",
+    homepage = "https://github.com/bacongobbler/draft-env",
+	version = version,
+	useTunnel = false,
+    packages = {
+        {
+            os = "darwin",
+            arch = "amd64",
+            url = "https://github.com/bacongobbler/draft-env/archive/v2.0.0.tar.gz",
+            sha256 = "abc010adcf48533904647d4633fb5d3eb36a92668e7a82df157cef10f98a61cf",
+            path = "draft-env-2.0.0/env.sh",
+        },
+        {
+            os = "linux",
+            arch = "amd64",
+            url = "https://github.com/bacongobbler/draft-env/archive/v2.0.0.tar.gz",
+            sha256 = "abc010adcf48533904647d4633fb5d3eb36a92668e7a82df157cef10f98a61cf",
+            path = "draft-env-2.0.0/env.sh",
+        },
+        {
+            os = "windows",
+            arch = "amd64",
+            url = "https://github.com/bacongobbler/draft-env/archive/v2.0.0.tar.gz",
+            sha256 = "abc010adcf48533904647d4633fb5d3eb36a92668e7a82df157cef10f98a61cf",
+            path = "draft-env-2.0.0/env.ps1",
+        }
+    }
+}
